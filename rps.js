6d6b2231ad079cd043c6438eed3ca4003a7fe184
Alex Burke <alexlburke@gmail.com>
2022-04-15 17:21:34 -0400

    created and tested  playRound function

--- rps.js
+++ rps.js
@@ -1,19 +1,43 @@
-function computerPlay(comp) {
+function computerPlay() {
   let choice = Math.floor(Math.random() * 3); 
   if (choice == 0) {
-    return "Rock";
+    return "rock";
   }
   else if (choice == 1) {
-    return "Paper";
+    return "paper";
   }
   else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-computerPlay();
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "It's a tie!"
+  }
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock breaks scissors."
+  }
+  if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win! Scissors cut paper."
+  }
+  if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper covers rock."
+  }
+  if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Scissors are broken by rock."
+  }
+  if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose! Paper is cut by scissors."
+  }
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose! Rock is covered by paper."
+  }
+}
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 
 
   
